2f15788e74eac12b5764a6a50db75c8d57c99d75
Jag Reehal <jag.reehal@gmail.com>
2015-07-10 00:53:54 +0100

    More object examples

--- src/2-almost-everything-is-a-object/objects.spec.js
+++ src/2-almost-everything-is-a-object/objects.spec.js
@@ -1,11 +1,11 @@
 const assert = require('chai').assert;
 
 describe('Almost everything is an object', function () {
-  it('Even an object literal', ()=> {
+  it('Object literal', ()=> {
     let o = {};
 
     assert.isObject({});
-    assert.equal(Object.getPrototypeOf(o), Object.prototype);
+    assert.deepEqual(Object.getPrototypeOf(o), Object.prototype);
 
     // yikes!
     assert.ok(o.constructor);
@@ -19,6 +19,57 @@ describe('Almost everything is an object', function () {
 
     // yay!
     assert.isNotFunction(o.constructor);
+  });
+
+  it('A function can return an object', ()=> {
+    let o = Object.create(null);
+
+    assert.isObject(o);
+    assert.deepEqual(null, Object.getPrototypeOf(o));
+  });
+
+  it('A function constructor', ()=> {
+    function O(name) {
+      this.name = name;
+      this.getName = ()=> {
+        return this.name;
+      };
+    }
+
+    let o = new O('x');
+    assert.equal(o.getName(), 'x');
+    assert.isObject(o);
+    assert.deepEqual(Object.getPrototypeOf(o), Object.prototype);
+  });
+
+  it('A function constructor v2', ()=> {
+    function O(name) {
+      this.name = name;
+    }
+
+    O.prototype.getName = function () {
+      return this.name;
+    };
+
+    let o = new O('x');
+    assert.equal(o.getName(), 'x');
+    assert.isObject(o);
+    assert.deepEqual(Object.getPrototypeOf(o), O.prototype);
+  });
+
+  it('Object.create', ()=> {
+    const o = {
+      getName () {
+        return this.name;
+      }
+    };
+
+    let o1 = Object.create(o);
+    o1.name = 'x';
+
+    assert.equal(o1.getName(), 'x');
+    assert.isObject(o1);
+    assert.deepEqual(Object.getPrototypeOf(o1), o);
   });
 
 });
